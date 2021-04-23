e988ed915d9a6eac9dd13248d8859d38582d44d3
mgcarbonell <mgcarbonell@gmail.com>
2021-04-22 21:36:37 -0700

    Finishes class method

--- data-structs/ES2015-class-syntax.js
+++ data-structs/ES2015-class-syntax.js
@@ -74,7 +74,7 @@ class Point {
     this.x = x;
     this.y = y;
   }
-
+  // Class method (see line 89)
   static distance(a, b) {
     const dx = a.x - b.x;
     const dy = a.y - b.y;
@@ -85,3 +85,6 @@ class Point {
 
 const p1 = new Point(5, 5);
 const p2 = new Point(10, 10);
+
+console.log(Point.distance(p1, p2))
+
