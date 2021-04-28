41bd0b12d2d1190caebf0789dabd26c8bf34535d
mgcarbonell <mgcarbonell@gmail.com>
2021-04-28 14:45:28 -0700

    Completes stack (linked list) implementation

--- data-structs/stacksAndQueues.js
+++ data-structs/stacksAndQueues.js
@@ -3,7 +3,48 @@ Objectives:
 What is a stack
 Understand use cases for a stack
 Implement operations on a stack data structure
-This will be an array implementation.
+This will be a Linked List implementation
 */
 
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
 
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (!this.first) return null;
+
+    let temp = this.first;
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
